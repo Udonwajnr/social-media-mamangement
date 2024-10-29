e12e7fa0682d783709c871fc0aa7d1ba55d974c2
Umoh Udonwa <udonwajnr10@gmail.com>
2024-10-29 14:57:54 +0100

    commit

--- app/terms-of-service/page.js
+++ app/terms-of-service/page.js
@@ -1,10 +1,8 @@
-import Navbar from '@/components/navbar'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default function TermsOfService() {
   return (
     <div className="min-h-screen bg-background">
-      <Navbar />
       <main className="container mx-auto px-4 py-8">
         <Card>
           <CardHeader>
